a96ed540546d8407ad1419e874f6cf49de2908dc
UserEdmund <mineedmund@163.com>
2026-06-12 16:09:55 +0800

    changed blog content

--- posts.js
+++ posts.js
@@ -13,7 +13,9 @@ Working under Prof. Tun Lu, I explored how to help vehicles track swimmers more 
 
 The core idea was using **vanishing point-based optimization** during the Non-Maximum Suppression process. When objects are far from the camera, standard pixel-space assumptions become brittle; geometric context can help the detector make more stable decisions.
 
-This work connected computer vision with a very physical robotics problem: keeping perception reliable when the environment is moving, reflective, and noisy.
+It is more a work of increasing recall in this specific circumstance. We don't want to miss any suspicous case of drowning in the distance, will still keeping the accuracy of the swimmers that are near. 
+### Future work
+I would say the use of monocular depth graph prediction method would be better to deal with this situation. With depth prediction plus object detection, it would be more precise than just using vanishing-point based depth estimation.
         `
     }
 ];
